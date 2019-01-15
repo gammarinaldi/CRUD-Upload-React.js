a68dbd96e44fc76570f7b5177ffb55c773945606
gammarinaldi <gammarinaldi@yahoo.com>
2019-01-15 10:59:48 +0700

    Update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import ManageBrand from './components/ManageBrand';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ManageBrand />
+      </div>
+    );
+  }
+}
+
+export default App;
